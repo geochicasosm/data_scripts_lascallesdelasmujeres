fffc4f22cadef60596d09294c2549d5b08e0ed3d
Jessica Sena <jessisena@gmail.com>
2018-05-02 23:25:03 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ wikipedia/wikipediaJSON2.js
@@ -0,0 +1,27 @@
+module.exports.wikipediaJSON2 = 
+[
+    {
+      itemLabel: "Michelle Bachelet",
+      sitelink: "https://es.wikinews.org/wiki/Categor%C3%ADa:Michelle_Bachelet"
+    },
+    {
+      itemLabel: "Michelle Bachelet",
+      sitelink: "https://es.wikipedia.org/wiki/Michelle_Bachelet"
+    },
+    {
+      itemLabel: "Michelle Bachelet",
+      sitelink: "https://es.wikiquote.org/wiki/Michelle_Bachelet"
+    },
+    {
+      itemLabel: "Michelle Bachelet",
+      sitelink: "https://es.wikisource.org/wiki/Autor:Michelle_Bachelet"
+    },
+    {
+      itemLabel: "Alice Greene",
+      sitelink: "https://es.wikipedia.org/wiki/Alice_Greene"
+    },
+    {
+      itemLabel: "Kerstin Szymkowiak",
+      sitelink: "https://es.wikipedia.org/wiki/Kerstin_Szymkowiak"
+    }
+];
